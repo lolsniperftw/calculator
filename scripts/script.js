e7585598110d489900b020503d99f5a6b9ed3f33
Oran Mehavi <oranhero@gmail.com>
2022-08-13 23:15:55 +0300

    Finished calculator script

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,51 @@
 //get the buttons grid
 const buttGrid = document.querySelector('.buttons-grid');
 
+//get the display
+const display = document.querySelector('#display');
 
+//get the numeric buttons
+const numericButt = document.querySelectorAll('.number');
 
-function operate (numbers, operator) {
+//get the operator buttons
+const operatorButt = document.querySelectorAll('.operator');
+
+//get equals
+const equal = document.querySelector('.equals');
+
+//get clear
+const clear = document.querySelector('.clear');
+
+//get backspace
+const backspace = document.querySelector('.backspace');
+
+//get dot button
+const dot = document.querySelector('.dot');
+
+let operator = '';
+let numbers = [];
+let canInsertSecondNumber = false;
+let isDotPressed = false;
+
+function operate(numbers, operator) {
+    let result;
     switch (operator) {
         case '+':
-            add(numbers[0], numbers[1]);
+            result = add(Number(numbers[0]), Number(numbers[1]));
             break;
         case '-':
-            sub(numbers[0], numbers[1]);
+            result = sub(numbers[0], numbers[1]);
             break;
         case '*':
-            multiply(numbers[0], numbers[1]);
+            result = multiply(numbers[0], numbers[1]);
             break;
         case '/':
-            divide(numbers[0], numbers[1]);
+            result = divide(numbers[0], numbers[1]);
+            result = Math.round(result * 100) / 100;
             break;
     }
+
+    return result;
 }
 function add(num1, num2) {
     return num1 + num2;
@@ -35,8 +63,79 @@ function divide(num1, num2) {
     return num1 / num2;
 }
 
+function addToDisplay(digit) {
+
+    if (display.textContent === '0') {
+        display.textContent = digit;
+    }
+    else if (display.textContent.length < 8) {
+        display.textContent += digit;
+    }
+
+    if (canInsertSecondNumber) {
+        canInsertSecondNumber = false;
+        display.textContent = digit;
+        isDotPressed = false;
+    }
+
+}
 
 
 
-//create the calculator
+//add event listener to the numbers
+numericButt.forEach(button => {
+    button.addEventListener('click', (event) => {
+        addToDisplay(event.target.textContent);
+    })
+});
+
+operatorButt.forEach(button => {
+    button.addEventListener('click', (event) => {
+        if (!operator) {
+            operator = event.target.textContent;
+            canInsertSecondNumber = true;
+            numbers.push(display.textContent);
+        }
+    })
+});
+
+equal.addEventListener('click', () => {
+    
+    if (operator === '/' && display.textContent === '0') {
+        alert('Cannot divide by zero!');
+    }
+    else if (operator) {
+        numbers.push(display.textContent);
+        display.textContent = operate(numbers, operator);
+        operator = '';
+        numbers = [];
+    }
+    
+});
+
+clear.addEventListener('click', () => {
+    display.textContent = '0';
+    operator = '';
+    numbers = [];
+});
+
+backspace.addEventListener('click', () => {
+    let currentNumber;
+    currentNumber = display.textContent;
+    if (currentNumber.length === 1) {
+        display.textContent = '0';
+    }
+    else {
+        display.textContent = currentNumber.slice(0, currentNumber.length - 1);
+    }
+});
+
+dot.addEventListener('click', (event) =>  {
+
+    if (!isDotPressed) {
+        display.textContent += event.target.textContent;
+        isDotPressed = true;
+    }
+
+});
 
